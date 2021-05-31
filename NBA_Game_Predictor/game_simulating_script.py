03975ab32bb454c69bf43f2572fdeaf95836c953
aryankix101 <aryankumawat33@gmail.com>
2021-05-31 10:37:35 -0400

    Deleted files

--- NBA_Game_Predictor/game_simulating_script.py
+++
@@ -1,9 +0,0 @@
-from GameSimulator import *
-import numpy as np
-
-gsw = Team('BOS',[90,100,110,120],[85,90,100,110])
-cle = Team('CLE',[90,99,108,117],[85,90,100,110])
-
-msim = MatchupSimulator(gsw,cle)
-
-msim.gamesSim(1000)
